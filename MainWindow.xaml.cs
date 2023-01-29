f0e7e7800db529cfd1678e5761c9a8fb831981a2
ouch <ouch@HOME-PC>
2023-01-29 22:36:52 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+
+namespace RPN_Calc
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void СalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            RpnExpr.Text = RPN.GetExpression(expression.Text);
+            Result.Text = RPN.Counting(RpnExpr.Text).ToString();
+        }
+    }
+}
+
+
+    
